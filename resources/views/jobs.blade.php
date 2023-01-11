60eaaf85e5b4c59caf9edb9bafe9c76cd5eca023
ZinoChan <devzinochan@gmail.com>
2023-01-11 23:12:01 +0100

    add jobs listing page

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+@section('content')
+
+    @include('partials._search')
+    <section class="py-20">
+        @unless(count($jobs) == 0)
+            <div class="grid grid-cols-3 gap-12">
+                @foreach ($jobs as $job)
+                    <a class="block" href="/jobs/{{ $job['id'] }}">
+                        <x-job-card :job="$job" />
+                    </a>
+                @endforeach
+            </div>
+        @else
+            <p>no jobs found</p>
+        @endunless
+    </section>
+@endsection
