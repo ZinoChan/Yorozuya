d56f32cbced6adf13723503e344cb343d60dfeb8
ZinoChan <devzinochan@gmail.com>
2023-01-11 23:16:13 +0100

    refactor: move header to new component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,12 +12,15 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
             integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
             crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
         <script src="https://cdn.tailwindcss.com"></script>
 
         <title>Yorozuya | Dev</title>
     </head>
 
     <body style="font-family: 'Kumbh Sans', sans-serif;" class="bg-[#f2f2f2]">
+      @include('partials._header') 
+        <main class="max-w-screen-xl px-4 md:px-6 mx-auto">
 
             @yield('content')
         </main>
