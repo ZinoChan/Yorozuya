61c37b9a801e82dcb7deba00318e9b054aad9f62
ZinoChan <devzinochan@gmail.com>
2023-01-12 17:31:28 +0100

    add job creation route

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [JobsController::class, 'index']);
-
+Route::get('/jobs/create', [JobsController::class, 'create']);
 Route::get('/jobs/{job}', [JobsController::class, 'show']);
