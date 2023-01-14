499bf2d1f5dc4746864c4f267ecf6dcb74580382
ZinoChan <devzinochan@gmail.com>
2023-01-14 21:57:13 +0100

    add del and edit job btns

--- resources/views/components/company-site.blade.php
+++ resources/views/components/company-site.blade.php
@@ -1,6 +1,21 @@
-@props(['company', 'website'])
+@props(['company', 'website', 'id'])
 
 <div class="relative -mt-10 flex bg-white rounded-md max-w-screen-md mx-auto">
+    <div class="absolute -top-4 right-4 flex items-center space-x-3">
+        <button class="text-white bg-blue-500 text-lg rounded py-1 px-2">
+            <a href="/jobs/{{ $id }}/edit">
+                <i class="fa-regular fa-pen-to-square"></i>
+            </a>
+        </button>
+
+        <form method="POST" action="/jobs/{{ $id }}">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="text-white bg-red-500 text-lg rounded py-1 px-2 ">
+                <i class="fa-solid fa-trash-can"></i>
+            </button>
+        </form>
+    </div>
     <div class="bg-pink-400 rounded-l-md h-40 w-40 p-4 flex items-center justify-center">
         <span class="capitalize text-white font-bold text-4xl">
             logo
