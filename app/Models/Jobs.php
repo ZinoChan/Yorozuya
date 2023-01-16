e47973577c079414ea0b4839b6afeac2f461075b
ZinoChan <devzinochan@gmail.com>
2023-01-16 19:34:27 +0100

    add user and job relation

--- app/Models/Jobs.php
+++ app/Models/Jobs.php
@@ -9,12 +9,17 @@ class Jobs extends Model
 {
     use HasFactory;
     
-    protected $fillable = ['company', 'position', 'logo', 'location', 'website', 'description', 'contract', 'expertise', 'expertiseTags', 'role', 'roleTags'];
+    protected $fillable = ['user_id', 'company', 'position', 'logo', 'location', 'website', 'description', 'contract', 'expertise', 'expertiseTags', 'role', 'roleTags'];
 
     public function scopeFilter($query, array $filters)
     {
         if ($filters["search"] ?? false) {
             $query->where("position", "like", "%" . request("search") . '%')->orWhere('description', 'like', '%' . request('search') . '%')->orWhere('role', 'like', '%' . request('search') . '%');
         }
+    }
+
+    //Relationship to user
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
