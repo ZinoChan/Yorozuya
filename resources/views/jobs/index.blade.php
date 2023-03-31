4936880a81d292c75c3994ca5066fdb46a207461
ZinoChan <devzinochan@gmail.com>
2023-03-31 11:53:53 +0000

    styles: responsive design

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,9 +4,9 @@
     @include('partials._search')
     <section class="py-20 max-w-screen-lg mx-auto">
         @unless(count($jobs) == 0)
-            <div class="grid grid-cols-3 gap-12">
+            <div class="grid gap-12 xl:grid-cols-3 md:grid-cols-2 grid-cols-1 items-stretch">
                 @foreach ($jobs as $job)
-                    <a class="block" href="/jobs/{{ $job['id'] }}">
+                    <a class="inline-flex" href="/jobs/{{ $job['id'] }}">
                         <x-job-card :job="$job" />
                     </a>
                 @endforeach
@@ -15,7 +15,7 @@
             <p>no jobs found</p>
         @endunless
         <div class="max-w-xs  mt-10 mx-auto">
-            {{$jobs->links()}}
+            {{ $jobs->links() }}
         </div>
     </section>
 @endsection
